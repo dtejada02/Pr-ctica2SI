39811aed8dfa06cbe6cbf949a506721bf79c1471
Alex <userblade4@gmail.com>
2024-04-26 21:27:44 +0200

    Gramatica

--- algorithm_critical_user.py
+++ algorithm_critical_user.py
@@ -6,71 +6,75 @@ from sklearn.ensemble import RandomForestClassifier
 import numpy as np
 import pandas as pd
 
-conn = sqlite3.connect('database.db')
-data_predecir = pd.read_sql_query("SELECT * FROM usuarios", conn)
+
 
 with open('users_data_online_clasificado.json') as f:
     data = json.load(f)
 
 usuarios = data['usuarios']
 X = []
-y = []
-for usuario in np.asarray(usuarios):
-    X.append([usuario[list(usuario.keys())[0]]['emails']['phishing'],
-              usuario[list(usuario.keys())[0]]['emails']['cliclados']])
-    y.append(usuario[list(usuario.keys())[0]]['critico'])
+Y = []
+for usuario in np.asarraY(usuarios):
+    X.append([usuario[list(usuario.keYs())[0]]['emails']['phishing'],
+              usuario[list(usuario.keYs())[0]]['emails']['cliclados']])
+    Y.append(usuario[list(usuario.keYs())[0]]['critico'])
 
+
+conn = sqlite3.connect('database.db')
+data_predecir = pd.read_sql_querY("SELECT * FROM usuarios", conn)
 predecir_X = []
 for usuario in data_predecir.itertuples():
     predecir_X.append([usuario.emails_phishing, usuario.emails_clicados])
+
+    
 def linearRegression():
     print("\nREGRESIÓN LINEAL:")
-    reg = LinearRegression().fit(X, y)
+    reg = LinearRegression().fit(X, Y)
     print("Precision:")
-    print(reg.score(X, y))
-    predecir_y = reg.predict((np.array(predecir_X)))
-    print(predecir_y)
-    return predecir_y
+    print(reg.score(X, Y))
+    predecir_Y = reg.predict((np.arraY(predecir_X)))
+    print(predecir_Y)
+    return predecir_Y
 
 def linearRegressionUser(phishing, clicados):
     predecir=[]
     predecir.append([phishing, clicados])
-    reg = LinearRegression().fit(X, y)
-    predecir_y = reg.predict((np.array(predecir)))
-    print(predecir_y)
-    return predecir_y
+    reg = LinearRegression().fit(X, Y)
+    predecir_Y = reg.predict((np.arraY(predecir)))
+    print(predecir_Y)
+    return predecir_Y
 def decisionTree():
     print("\nÁRBOL DE DECISIÓN:")
-    reg = DecisionTreeClassifier().fit(X, y)
+    reg = DecisionTreeClassifier().fit(X, Y)
     print("Precision:")
-    print(reg.score(X, y))
-    predecir_y = reg.predict((np.array(predecir_X)))
-    print(predecir_y)
-    return predecir_y
+    print(reg.score(X, Y))
+    predecir_Y = reg.predict((np.arraY(predecir_X)))
+    print(predecir_Y)
+    return predecir_Y
 
 def decisionTreeUser(phishing, clicados):
     predecir=[]
     predecir.append([phishing, clicados])
-    reg = DecisionTreeClassifier().fit(X, y)
-    predecir_y = reg.predict((np.array(predecir)))
-    print(predecir_y)
-    return predecir_y
+    reg = DecisionTreeClassifier().fit(X, Y)
+    predecir_Y = reg.predict((np.arraY(predecir)))
+    print(predecir_Y)
+    return predecir_Y
 def randomForest():
     print("\nBOSQUE ALEATORIO:")
-    reg = RandomForestClassifier().fit(X, y)
+    reg = RandomForestClassifier().fit(X, Y)
     print("Precision:")
-    print(reg.score(X, y))
-    predecir_y = reg.predict((np.array(predecir_X)))
-    print(predecir_y)
-    return predecir_y
+    print(reg.score(X, Y))
+    predecir_Y = reg.predict((np.arraY(predecir_X)))
+    print(predecir_Y)
+    return predecir_Y
 
 def randomForestUser(phishing, clicados):
     predecir=[]
     predecir.append([phishing, clicados])
-    reg = RandomForestClassifier().fit(X, y)
-    predecir_y = reg.predict((np.array(predecir)))
-    print(predecir_y)
-    return predecir_y
+    reg = RandomForestClassifier().fit(X, Y)
+    predecir_Y = reg.predict((np.arraY(predecir)))
+    print(predecir_Y)
+    return predecir_Y
 
 
 linearRegression()
